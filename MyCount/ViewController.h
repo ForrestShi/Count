d114e0743c0145590710bd288870bc0959ba074d
Forrest <forrest.shi@gmail.com>
2013-10-12 16:52:07 +0800

    Initial Commit

---
+++ MyCount/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MyCount
+//
+//  Created by Shi Lin on 10/12/13.
+//  Copyright (c) 2013 Forrest Shi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
